663ca6aa6c299d94f5b78540cbba2d157400024f
Jesy05 <jgonzalezj@uamv.edu.ni>
2026-04-20 15:33:40 -0600

    Primer commit: Tarea de la Semana 6

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] edades = new int[10];
+        int edad;
+
+        System.out.println("Ingresa 10 edades:");
+
+        for(int i = 0; i < 10; i++){
+            edad = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "Ingresa la edad #" + (i + 1) + ": "));
+            edades[i] = edad;
+        }
+    }
+}
